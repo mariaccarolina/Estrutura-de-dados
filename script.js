633d1f135914a916d1311d13f87e6ae6ffde9fae
Maria Carolina <demycar24@gmail.com>
2022-07-18 19:56:21 -0300

    Estrutura de dados

---
+++ script.js
@@ -0,0 +1,32 @@
+
+// Crie uma variável que receba um nome;
+let nome="Dafne"
+console.log (nome)
+
+
+// Crie uma variável que receba um número;
+let numero=6
+console.log (numero)
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+let ano=2022- numero
+console.log (ano)
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+let soma= numero + ano
+console.log (soma)
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+
+console.log ("Estou mexendo no console.log")
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+let quartaFeira=true
+console.log (quartaFeira)
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log (typeof quartaFeira)
+
+console.log (typeof nome)
+
+console.log (typeof soma)
